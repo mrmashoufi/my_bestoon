900f545a1843025ce161a89bdf6c90f63317cd93
mrm <mr.mashoufi.95@gmail.com>
2016-12-26 01:47:23 +0330

    Ezafe kardan model kharj

--- mainwebapp/models.py
+++ mainwebapp/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class kharj(models.Model):
+    Onvan = models.CharField(max_length = 255)
+    Zaman = models.DateTimeField()
+    Meghdar = models.BigIntegerField()
+    Karbar = models.ForeignKey(User)
